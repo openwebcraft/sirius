25b8b471cff2cb11320dfabb10ae621923f57c06
Matthias Geisler <matthias@openwebcraft.com>
2016-03-23 09:59:06 +0100

    fix UTF-8 support, 2nd
    https://github.com/genmon/sirius/issues/13

--- sirius/coding/templating.py
+++ sirius/coding/templating.py
@@ -18,4 +18,4 @@ ENV = jinja2.Environment()
 
 def default_template(raw_html):
     t = ENV.from_string(DEFAULT_TEMPLATE)
-    return t.render(raw_html=raw_html)
+    return t.render(raw_html=raw_html).encode('utf-8')
