05f31ad6d62aa6136f1bfc6a4ffc9b579e634f20
Matthias Geisler <matthias@openwebcraft.com>
2016-03-23 10:00:15 +0100

    fix UTF-8 support
    https://github.com/genmon/sirius/issues/13

--- sirius/web/printer_print.py
+++ sirius/web/printer_print.py
@@ -122,7 +122,7 @@ def preview(user_id, username, printer_id):
     assert user_id == login.current_user.id
     assert username == login.current_user.username
 
-    message = flask.request.data
+    message = flask.request.data.decode('utf-8')
     pixels = image_encoding.default_pipeline(
         templating.default_template(message))
     png = io.BytesIO()
